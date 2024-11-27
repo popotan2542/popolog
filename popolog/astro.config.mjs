08a08dcfe047e546585174592785cdc703a089ac
popotan2542 <64960528+popotan2542@users.noreply.github.com>
2024-11-27 23:53:20 +0900

    feat:github pagesへのデプロイテスト

--- popolog/astro.config.mjs
+++ popolog/astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://popotan2542.github.io',
+	base: '/popolog',
 	integrations: [mdx(), sitemap()],
 });
